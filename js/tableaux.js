245e6e736e9626165dc7348660b28d1e41323c6e
Dendaletche <jodenda@gmail.com>
2021-01-13 20:11:37 +0100

    Create tableaux.js

---
+++ js/tableaux.js
@@ -0,0 +1,16 @@
+/*******************************************************************************
+0123456789          0123456789          0123456789          0123456789
+          0123456789          0123456789          0123456789          0123456789
+          
+          Format 80 colonnes
+          
+          Fichier javascript de creation de la classe tableau
+          définitions de ses attributs et méthodes
+          
+          Auteur : ...
+          Date : ...
+          Dans le cadre du projet Web de la licence MIAGE de Rodez 20/21
+         
+*******************************************************************************/
+
+// class Tableau 
